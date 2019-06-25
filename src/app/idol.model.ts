d68226616f95f6dc0d533db4e2d9a3f5010a8837
GimYoungPhil <youngphil.gim@gmail.com>
2019-06-25 10:23:39 +0900

    add idol.model

---
+++ src/app/idol.model.ts
@@ -0,0 +1,11 @@
+export class Idol {
+  constructor(
+    public name: string,
+    public group: string,
+    public company: string,
+    public birth: string,
+    public height: string,
+    public weight: string,
+    public imageUrl: string,
+  ) { }
+}
