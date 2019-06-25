80839aef268ecafed240f96a0c354fdf6e46ed77
GimYoungPhil <youngphil.gim@gmail.com>
2019-06-25 15:05:47 +0900

    fixed card

--- src/app/idol-card/idol-card-image.component.ts
+++ src/app/idol-card/idol-card-image.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-idol-card-image',
-  template: `<img class="card-img" [src]="url" alt="...">`,
-  styles: [`.card-img { object-fit: cover; min-height: 13rem; }`]
+  template: `<img class="card-img" [src]="url" alt="..." *ngIf="url">`,
+  styleUrls: ['./idol-card-image.component.css']
 })
 export class IdolCardImageComponent implements OnInit {
   @Input() url: string;
